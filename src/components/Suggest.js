c1c2f2d3c0dc463c1a4e507846d46e13a3945362
javitecsis <javitecsis@gmail.com>
2021-06-22 21:15:18 -0500

    add card priorities and Suggest

--- src/components/Suggest.js
+++ src/components/Suggest.js
@@ -1,19 +1,29 @@
 function Suggest () {
     return(
-        <div className="container">
-            <div>
-                <h3>Nomber:</h3>
-                <input placeholder="Pintar Postes"></input>
-                <h3>Prioridad</h3>
-                <button>X</button>                
-                <h3>Mejora</h3>
-                <button>x</button>
-                <button><strong>x</strong></button>
+        <div className="Content">
+            <div className="Content_card">
+              <div className="Content_head">
+                <div>
+                  <h3 className="Content_title">Nombre:</h3>
+                  <input className="Content_record"placeholder="Pintar Postes"></input>
+                </div>
+                <div className="Content_name_check">
+                  <h3 className="Content_title">Prioridad</h3>
+                  <h3 className="Content_title">Mejora</h3>
+                </div>
+                <div claseName="Content_button">
+                  <button type="button" className="Content_select">X</button><br/>
+                  <button type="button" className="Content_select"> </button>
+                </div>
+                <div>
+                   <button className="Content_close"><strong>x</strong></button>
+                </div> 
+              </div>
+              <div >
+                <h3 className="Content_title">Detalles:</h3>
+                <textarea className="Content_text">Pintar Postes</textarea>
+              </div>     
             </div>
-            <div>
-                <h3>Detalles:</h3>
-                <textarea>Pintar Postes</textarea>
-            </div>  
         </div>
     )
 }
