40df58a6ad0374c06b14a06dc3ead8464da66f9d
Jaime Daniel Piratova Castro <dan2005p@gmail.com>
2021-06-23 14:41:40 -0500

    reportFailure

---
+++ src/components/ReportFailure.js
@@ -0,0 +1,25 @@
+function ReportFailure () {
+    return(
+        <div className="Container_failure">
+            <div className="Content_failure">
+
+              <div className="main-failure">
+                <div>
+                  <h4 className="title-failure">Nombre:</h4>
+                  <input className="input-failure"placeholder="Bombillos rotos"></input>
+                </div>                 
+              </div>
+
+              <div >
+                <h4 className="title-failure">Detalles:</h4>
+                <textarea className="text-details">En el pasillo de la piscina no prenden lan luces al anochecer</textarea>
+              </div> 
+              <div>
+                <input className="button" type="submit" value="Reportar falla" />
+              </div>    
+            </div>
+        </div>
+    )
+}
+
+export default ReportFailure;
