94eeb3c7670fafe6fe997350649e9449770db740
javitecsis <javitecsis@gmail.com>
2021-06-23 08:48:25 -0500

    add component prioriAdmin and delete logos favicon

--- src/components/PrioriAdmin.js
+++ src/components/PrioriAdmin.js
@@ -1,18 +1,22 @@
 function Priori_Admin () {
     return(
-        <div className="Tank">
-          <div className="Tank_todo">
-             <h3>Por hacer</h3>
-            <div className="Tank_button">
-                <button><h3>Nombre</h3><h3>Detalles...</h3></button>
-                <button><h3>Nombre</h3><h3>Detalles...</h3></button>
+        <div className="Tanks">
+            <div className="Tanks_block">
+               <h3 className="Tanks_title">Por hacer</h3>
+              <div className="Tanks_content">
+                <button className="Tanks_button"><h3>Nombre</h3><h3>Detalles...</h3></button>
+                <button className="Tanks_button"><h4>Nombre</h4><h4>Detalles...</h4></button>
+              </div>
+                <button className="Tanks_create">Crear Card</button>
             </div>
-              <button className="Tank_create">Crear Card</button>
-              <h3>En proceso</h3>
-            <div className="Tank_todo"></div>
-              <h3>Terminadas</h3>
-            <div className="Tank_todo"></div>
-          </div>
+            <div className="Tanks_block">
+               <h3 className="Tanks_title">En proceso</h3>
+               <div className="Tanks_content"></div>
+            </div>
+            <div className="Tanks_block">
+               <h3 className="Tanks_title">Terminadas</h3>
+               <div className="Tanks_content"></div>
+            </div>                     
         </div>
     )
 }
